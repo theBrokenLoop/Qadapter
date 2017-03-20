891d18351eb4480003e561f79181d8c72c6d73e1
vipin14119 <vipin14119@iiitd.ac.in>
2017-03-20 20:33:44 +0530

    dynamic color non repetitive

--- plotData.m
+++ plotData.m
@@ -5,10 +5,10 @@ function plotData( X, y, levels )
     title('Aptence Level dataset distribution');
     xlabel('Average Time (1-60)');
     ylabel('Average Accuracy (1-100)');
+    CM = jet(length(levels));
     for l=levels
-        levels(1,l) = num2str(l);
         class = find(y == l);
-        plot(X(class, 1), X(class,2),'+','lineWidth', 2 ,'MarkerSize', 7', ...
+        plot(X(class, 1), X(class,2),'+','lineWidth', 2,'color', CM(l,:) ,'MarkerSize', 7', ...
             'DisplayName', sprintf('Level: %d', l));
     end
     legend('-DynamicLegend');
